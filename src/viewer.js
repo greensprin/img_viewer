85ad8f0a9bb0b65c15948e497e8a8c8dd7677aad
mmkmss <mngjxd03170317@gmail.com>
2020-07-29 00:21:44 +0900

    dataの値を変更して保持できるように変更 試しにshift処理を実装。まだおかしな点があるが、これから修正する。

--- src/viewer.js
+++ src/viewer.js
@@ -13,6 +13,12 @@ img.classList.add("imgClass");
 var img_div = document.createElement("div");
 img_div.appendChild(img);
 
+var data_canvas = document.createElement("canvas");
+data_canvas.classList.add("canvasData");
+
+var dc_div = document.createElement("div");
+dc_div.appendChild(data_canvas);
+
 // addEventListener ////////////////////////////////////////////////////////////
 // document
 document.ondragover = document.ondrop = function(e) { e.preventDefault(); }
@@ -55,6 +61,7 @@ var timeoutID;
 var win_width  = window.innerWidth;
 var win_height = window.innerHeight;
 var img_tmp_ary = ["", ""];
+var shift = 0;
 
 // function ////////////////////////////////////////////////////////////////////
 function doc_drop(e) {
@@ -68,19 +75,32 @@ function doc_drop(e) {
   img_child[img_index].src = e.dataTransfer.files[0].path;
   img_tmp_ary[img_index]   = e.dataTransfer.files[0].path;
 
-  // img.src = e.dataTransfer.files[0].path
-
   set_zoom();
 
   img_child[img_index].onload = function() {
+    set_canvas_data(img_index);
     draw_canvas_image();
   }
+}
+
+function set_canvas_data(img_index) {
+  var img_child = img_div.getElementsByClassName("imgClass");
+  var dc_child  = dc_div.getElementsByClassName("canvasData");
+
+  dc_child[img_index].width  = img_child[img_index].width;
+  dc_child[img_index].height = img_child[img_index].height;
+
+  var ctx = dc_child[img_index].getContext("2d");
+  ctx.drawImage(img_child[img_index], 0, 0);
 }
 
 function draw_canvas_image() {
   var num = pdiv.childElementCount;
   canvas_child = pdiv.getElementsByClassName("canvas_box");
-  img_child    = img_div.getElementsByClassName("imgClass");
+  // img_child    = img_div.getElementsByClassName("imgClass");
+
+  var dc_child  = dc_div.getElementsByClassName("canvasData");
+
   for (var i = 0; i < num; i++) {
     canvas_child[i].width  = win_width;
     canvas_child[i].height = win_height;
@@ -88,7 +108,8 @@ function draw_canvas_image() {
     ctx.imageSmoothingEnabled = false;
     ctx.clearRect(0,0,canvas_child[i].width,canvas_child[i].height);
     ctx.scale(size, size);
-    ctx.drawImage(img_child[i], objX, objY);
+    // ctx.drawImage(img_child[i], objX, objY);
+    ctx.drawImage(dc_child[i], objX, objY);
     ctx.scale(1/size, 1/size);
   }
 }
@@ -112,6 +133,8 @@ function set_zoom() {
 }
 
 function key_press(e) {
+  // console.log(e.keyCode);
+
   for (var i = 1; i <= 9; i++) {
     if (e.key == String(i)) {
       size = Number(e.key)
@@ -152,6 +175,11 @@ function key_press(e) {
       canvas_child.addEventListener("mousemove" , get_image_info, false);
       pdiv.appendChild(canvas_child);
 
+      // add new canvas data
+      var dc_child = document.createElement("canvas");
+      dc_child.setAttribute("class", "canvasData");
+      dc_div.appendChild(dc_child);
+
       // add new img
       var img_child = document.createElement("img");
       img_child.classList.add("imgClass");
@@ -175,6 +203,30 @@ function key_press(e) {
 
       draw_canvas_image();
     }
+  }
+
+  if (e.keyCode == 188) {
+    var num = img_div.childElementCount;
+    for (var i = 0; i < num; i++) {
+      set_canvas_data(i);
+    }
+
+    shift--;
+
+    shift_data();
+    draw_canvas_image();
+  }
+
+  if (e.keyCode == 190) {
+    var num = img_div.childElementCount;
+    for (var i = 0; i < num; i++) {
+      set_canvas_data(i);
+    }
+
+    shift++;
+
+    shift_data();
+    draw_canvas_image();
   }
 }
 
@@ -269,4 +321,34 @@ function cmp_image_out() {
   var bdiv = document.getElementById("button_div");
   var button_child = bdiv.getElementsByClassName("cmp_button");
   button_child[bnum].style.opacity = 0.5;
+}
+
+function shift_data() {
+  var num = dc_div.childElementCount;
+  var dc_child = dc_div.getElementsByClassName("canvasData");
+
+  for (var i = 0; i < num; i++) {
+    var width = dc_child[i].width;
+    var height = dc_child[i].height;
+
+    var ctx = dc_child[i].getContext("2d");
+    var img_data = ctx.getImageData(0, 0, width, height);
+
+    if (shift >= 0) {
+      for (var xy = 0; xy < width * height * 4; xy+=4) {
+        img_data.data[xy+0] = img_data.data[xy+0] << shift;
+        img_data.data[xy+1] = img_data.data[xy+1] << shift;
+        img_data.data[xy+2] = img_data.data[xy+2] << shift;
+      }
+    } else {
+      for (var xy = 0; xy < width * height * 4; xy+=4) {
+        img_data.data[xy+0] = img_data.data[xy+0] >> Math.abs(shift);
+        img_data.data[xy+1] = img_data.data[xy+1] >> Math.abs(shift);
+        img_data.data[xy+2] = img_data.data[xy+2] >> Math.abs(shift);
+      }
+    }
+
+    ctx.clearRect(0, 0, width, height);
+    ctx.putImageData(img_data, 0, 0);
+  }
 }
